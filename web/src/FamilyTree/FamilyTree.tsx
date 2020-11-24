3451e8c68e53483e8d9696122f8ec270e6491660
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2020-11-24 00:56:26 +0000

    feat: map dimensions to SVG canvas

--- web/src/FamilyTree/FamilyTree.tsx
+++ web/src/FamilyTree/FamilyTree.tsx
@@ -3,6 +3,7 @@ import {
   onePairSpouse,
   onePerson,
   onePairSpouseOneChild,
+  twoPairSpouseThreeChildren,
 } from '../__fixtures__/familyTree'
 import { PersonID, PersonSVGDimensions, RelatedPeople } from './types'
 
@@ -133,45 +134,92 @@ export function familyTreeToSVGDimensions(
   return firstPass
 }
 
+// export function FamilyTree() {
+//   const family = familyTreeToSVGDimensions(onePairSpouseOneChild)
+//   const lines = family.map((person) => person.svg.lines).flat()
+//   const circles = family.map((person) => person.svg.circle).flat()
+//   const texts = family.map((person) => person.svg.text).flat()
+
+//   console.log('>>> lines', lines)
+//   console.log('>>> circles', circles)
+//   console.log('>>> texts', texts)
+//   return (
+//     <svg
+//       version="1.1"
+//       baseProfile="full"
+//       width="100vw"
+//       height="100vh"
+//       xmlns="http://www.w3.org/2000/svg"
+//     >
+//       <rect width="100%" height="100%" fill="#222222" />
+
+//       {lines.map((line, idx) => (
+//         <line
+//           key={`line-${idx}`}
+//           x1={line.x1}
+//           x2={line.x2}
+//           y1={line.y1}
+//           y2={line.y2}
+//           stroke="rgb(255 255 255 / 60%)"
+//           strokeWidth={line.strokeWidth}
+//         />
+//       ))}
+
+//       {circles.map((c, idx) => (
+//         <circle key={`circle-${idx}`} cx={c.cx} cy={c.cy} fill="green" r={60} />
+//       ))}
+
+//       {texts.map((t, idx) => (
+//         <text key={`text-${idx}`} x={t.x} y={t.y} fill="white">
+//           {t.value}
+//         </text>
+//       ))}
+//     </svg>
+//   )
+// }
+
+function mapDimensionsToSVGCanvas(
+  dims: familyChartDimensions[]
+): familyChartDimensions[] {
+  const offsetRowHeight = 200
+  const offsetColumnWidth = 200
+  const rowHeight = 250
+  const columnWidth = 300
+
+  const minCol = Math.min(...dims.map((d) => d.column))
+  const minRow = Math.min(...dims.map((d) => d.row))
+  const adjCol = -minCol
+  const adjRow = -minRow
+  return dims.map((d) => ({
+    id: d.id,
+    row: (d.row + adjRow) * rowHeight + offsetRowHeight,
+    column: (d.column + adjCol) * columnWidth + offsetColumnWidth,
+  }))
+}
+
 export function FamilyTree() {
-  const family = familyTreeToSVGDimensions(onePairSpouseOneChild)
-  const lines = family.map((person) => person.svg.lines).flat()
-  const circles = family.map((person) => person.svg.circle).flat()
-  const texts = family.map((person) => person.svg.text).flat()
-  console.log('>>> lines', lines)
-  console.log('>>> circles', circles)
-  console.log('>>> texts', texts)
+  const dimensions = mapDimensionsToSVGCanvas(
+    familyTreeToChartDimensions(twoPairSpouseThreeChildren)
+  )
   return (
-    <svg
-      version="1.1"
-      baseProfile="full"
-      width="100vw"
-      height="100vh"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <rect width="100%" height="100%" fill="#222222" />
-
-      {lines.map((line, idx) => (
-        <line
-          key={`line-${idx}`}
-          x1={line.x1}
-          x2={line.x2}
-          y1={line.y1}
-          y2={line.y2}
-          stroke="rgb(255 255 255 / 60%)"
-          strokeWidth={line.strokeWidth}
-        />
-      ))}
-
-      {circles.map((c, idx) => (
-        <circle key={`circle-${idx}`} cx={c.cx} cy={c.cy} fill="green" r={60} />
-      ))}
-
-      {texts.map((t, idx) => (
-        <text key={`text-${idx}`} x={t.x} y={t.y} fill="white">
-          {t.value}
-        </text>
-      ))}
-    </svg>
+    <div>
+      <svg
+        version="1.1"
+        baseProfile="full"
+        width="100vw"
+        height="100vh"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect width="100%" height="100%" fill="#222222" />
+        {dimensions.map((d) => (
+          <>
+            <circle cx={d.column} cy={d.row} fill="green" r={60} />
+            <text x={d.column - 20} y={d.row + 5} fill="white">
+              {firstNLetters(twoPairSpouseThreeChildren[d.id].name, 6)}
+            </text>
+          </>
+        ))}
+      </svg>
+    </div>
   )
 }
