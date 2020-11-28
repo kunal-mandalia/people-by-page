1908ef5072a39b7dd0b4799d6d11d1639e421703
Kunal Mandalia <kunal.v.mandalia@gmail.com>
2020-11-28 00:33:38 +0000

    feat: initial page slider

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import { FamilyTree } from './FamilyTree/FamilyTree'
+import { PageSlider } from './PageSlider/PageSlider'
 
 function App() {
   return (
     <div className="App">
       <header>
+        <PageSlider firstPage={17} lastPage={985} />
         <FamilyTree />
       </header>
     </div>
